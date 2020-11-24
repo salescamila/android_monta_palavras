23dc0235c70854270612b8bda41d6e2b9ee04910
salescamila <salescamila1@gmail.com>
2020-11-24 11:33:08 -0400

    projecto criado

---
+++ app/src/main/java/com/example/monta_palavras/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.monta_palavras
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
